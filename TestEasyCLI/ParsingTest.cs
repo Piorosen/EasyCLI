5bf45d0ae094a37f7de0c3ed47a671924467d842
Piorosen <aoikazto@gmail.com>
2021-02-01 19:04:56 +0900

    프로젝트 생성 및 Parsing구문 작성, 유닛 테스트 추가.

---
+++ TestEasyCLI/ParsingTest.cs
@@ -0,0 +1,68 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestEasyCLI
+{
+    [TestClass]
+    public class ParsingTest
+    {
+        [TestMethod]
+        public void SimpleTest()
+        {
+            var r = new EasyCLI.EasyCLI().ParseArguments("a b c d e");
+            if (r.Count != 5)
+            {
+                Assert.Fail("개수가 5개가 아님.");
+            }
+
+
+            if (r[0] != "a" ||
+                r[1] != "b" ||
+                r[2] != "c" ||
+                r[3] != "d" ||
+                r[4] != "e")
+            {
+                Assert.Fail("실패함.");
+            }
+        }
+
+        [TestMethod]
+        public void BlankTest()
+        {
+            var r = new EasyCLI.EasyCLI().ParseArguments("   a    b    c   d e       ");
+            if (r.Count != 5)
+            {
+                Assert.Fail("개수가 5개가 아님.");
+            }
+
+
+            if (r[0] != "a" ||
+                r[1] != "b" ||
+                r[2] != "c" ||
+                r[3] != "d" ||
+                r[4] != "e")
+            {
+                Assert.Fail("실패함.");
+            }
+        }
+
+
+        [TestMethod]
+        public void HeavyTest()
+        {
+            var r = new EasyCLI.EasyCLI().ParseArguments("aaaa asf \"ha aaaa aa\" ddd\\t \'aaa d \" \" \\\'\' aa");
+            if (r.Count != 6)
+            {
+                Assert.Fail("개수가 6개가 아님.");
+            }
+            if (r[0] != "aaaa" ||
+                r[1] != "asf" ||
+                r[2] != "ha aaaa aa" ||
+                r[3] != "ddd\t" ||
+                r[4] != "aaa d \" \" \'" ||
+                r[5] != "aa")
+            {
+                Assert.Fail("데이터를 잘못 추출 함.");
+            }
+        }
+    }
+}
