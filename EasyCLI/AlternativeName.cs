9a7009cde295f0a31515bfd70dd16f8c3a814599
Piorosen <aoikazto@gmail.com>
2021-02-06 20:50:54 +0900

    주석을 추가함.

--- EasyCLI/AlternativeName.cs
+++ EasyCLI/AlternativeName.cs
@@ -1,9 +1,23 @@
 ﻿using System;
 namespace EasyCLI
 {
+    /// <summary>
+    /// EasyCLI와 함께 동작하며, 명령어에서 이름의 대체제로 사용이 됩니다. <br />
+    /// 1. Class Name<br />
+    /// 2. Method Name<br />
+    /// 3. Paramerter Name<br />
+    /// 위의 조건만 사용 가능합니다.
+    /// </summary>
     public class AlternativeNameAttribute : Attribute
     {
+        /// <summary>
+        /// 객체의 대체할 이름 입니다.
+        /// </summary>
         public string Name { get; private set; }
+        /// <summary>
+        /// 생성자 입니다.
+        /// </summary>
+        /// <param name="name">객체의 이름을 대체할 이름 입니다.</param>
         public AlternativeNameAttribute(string name = null)
         {
             Name = name;
--- EasyCLI/EasyCLI.cs
+++ EasyCLI/EasyCLI.cs
@@ -11,14 +11,27 @@ namespace EasyCLI
     {
         readonly List<object> classList = new List<object>();
 
+        /// <summary>
+        /// EasyCLI를 동작하는데 필요한 설정 입니다.
+        /// </summary>
         public Option Option { get; private set; } = Option.Instance;
 
         #region Class 추가 및 삭제 기능
+        /// <summary>
+        /// 객체를 추가 합니다.
+        /// </summary>
+        /// <param name="item">command에 명령어를 추가 합니다.</param>
         public void AddClass(object item)
         {
             classList.Add(item);
         }
 
+        /// <summary>
+        /// 디폴트 생성자를 이용하여 객체를 추가 합니다. <br />
+        /// 실패할 경우 false, 성공적으로 추가할 경우 true을 반환 합니다.
+        /// </summary>
+        /// <param name="type">command에 명령어를 추가 합니다.</param>
+        /// <returns>데이터 삽입의 성공 여부 입니다.</returns>
         public bool AddClass(Type type)
         {
             try
@@ -34,11 +47,19 @@ namespace EasyCLI
             return false;
         }
 
+        /// <summary>
+        /// 객체를 삭제 합니다.
+        /// </summary>
+        /// <param name="item">객체를 입력 받습니다.</param>
         public void RemoveClass(object item)
         {
             classList.Remove(item);
         }
-
+        
+        /// <summary>
+        /// 객체의 타입명을 기준으로 객체를 삭제 합니다.
+        /// </summary>
+        /// <param name="name">타입의 이름을 받습니다.</param>
         public void RemoveClass(string name)
         {
             object[] obj = ParsingArguments.FindClassObject(name, classList);
@@ -49,12 +70,25 @@ namespace EasyCLI
         }
         #endregion
 
-
+        /// <summary>
+        /// Call<object>(command) 로 동작합니다.
+        /// Example) Console WriteLine --value \"hello\"
+        /// 극단적인 예시 입니다.
+        /// </summary>
+        /// <param name="command">명령어를 수행할 커맨드 입니다.</param>
+        /// <returns>Object를 반환 합니다.</returns>
         public object Call(string command)
         {
             return Call<object>(command);
         }
 
+        /// <summary>
+        /// Example) Console WriteLine --value \"hello\"
+        /// 극단적인 예시 입니다.
+        /// </summary>
+        /// <typeparam name="T">반환할 타입입니다.</typeparam>
+        /// <param name="command">명령어를 수행할 커맨드 입니다.</param>
+        /// <returns>T를 반환 합니다.</returns>
         public T Call<T>(string command)
         {
             var list = ParsingArguments.Result(command, classList);
